64a3d28780c2d7e644e4869bf4777cdacf603c76
William Rice <william.rice192@gmail.com>
2018-04-23 15:25:15 -0400

    Initial commit

---
+++ src/main/java/co/williamrice/courses/Main.java
@@ -0,0 +1,53 @@
+package co.williamrice.courses;
+
+
+import co.williamrice.courses.model.CourseIdea;
+import co.williamrice.courses.model.CourseIdeaDAO;
+import co.williamrice.courses.model.SimpleCourseIdeaDAO;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.get;
+import static spark.Spark.post;
+import static spark.Spark.staticFileLocation;
+
+public class Main {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+
+        CourseIdeaDAO dao = new SimpleCourseIdeaDAO();
+
+        get("/", (req, res) -> {
+            Map<String, String> model = new HashMap<>();
+            model.put("username", req.cookie("username"));
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/sign-in", ((req, res) -> {
+            Map<String, String> model = new HashMap<>();
+            String username = req.queryParams("username");
+            res.cookie("username", username);
+            model.put("username", username);
+            res.redirect("/");
+            return null;
+        }));
+
+        get("/ideas", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("ideas", dao.findAll());
+            return new ModelAndView(model, "ideas.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/ideas", (req, res) -> {
+            String title = req.queryParams("title");
+            CourseIdea courseIdea = new CourseIdea(title,
+                    req.cookie("username"));
+            dao.add(courseIdea);
+            res.redirect("/ideas");
+            return null;
+        });
+    }
+}
---
+++ src/main/java/co/williamrice/courses/model/CourseIdeaDAO.java
@@ -0,0 +1,9 @@
+package co.williamrice.courses.model;
+
+import java.util.List;
+
+public interface CourseIdeaDAO {
+    boolean add(CourseIdea idea);
+
+    List<CourseIdea> findAll();
+}
